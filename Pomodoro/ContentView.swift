b17142bf7ddda55370d5aa156183f8c7eebe6081
Ncolvvs <nicolassantiago.5050@gmail.com>
2024-06-28 15:25:10 -0400

    Initial Commit

---
+++ Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pomodoro
+//
+//  Created by Nicolas Santiago on 28-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Nicolas Santiago on 28-06-24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
